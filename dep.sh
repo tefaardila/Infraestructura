f1580928dcc49f85f32a6ac6e4c9244f9ed58c7c
tefaardila <40218397+tefaardila@users.noreply.github.com>
2021-05-24 18:35:59 -0500

    Update dep.sh
    changing directory files

--- dep.sh
+++ dep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Hello World"
-sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_SergioAra.Infraestructura-TI/drop/files/index.html /var/www/html/index.nginx-debian.html
-sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_SergioAra.Infraestructura-TI/drop/files/. /var/www/html/
+sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_tefaardila.Infraestructura/drop/files/index.html /var/www/html/index.nginx-debian.html
+sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_tefaardila.Infraestructura/drop/files/. /var/www/html/
 echo "Files moved"
