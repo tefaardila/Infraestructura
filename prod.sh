0eb2ce939d761427c8f02f99452283534a9b591d
tefaardila <40218397+tefaardila@users.noreply.github.com>
2021-05-24 13:23:05 -0500

    Add files via upload
    First

---
+++ prod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello World"
+sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_SergioAra.Infraestructura-TI/drop/files/. /home/azureuser/html/web1/
+sudo cp -r -f /home/azureuser/azagent/_work/r1/a/_SergioAra.Infraestructura-TI/drop/files/. /home/azureuser/html/web2/
+echo "Files moved"
